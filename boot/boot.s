dd97dd6af7b4d06378de90f740969df166aefbad
itszubariel <anishchapagain00@gmail.com>
2026-06-11 22:13:13 +0900

    fix: add timer interrupt handler and keep multiboot header
    Implemented the low-level assembly for timer interrupts and ensured the multiboot header is kept by the linker.

--- boot/boot.s
+++ boot/boot.s
@@ -166,3 +166,27 @@ long_mode_start:
     pop rcx
     pop rax
     iretq
+
+    global timer_interrupt_handler
+    timer_interrupt_handler:
+    push rax
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    extern timer_handler_inner
+    call timer_handler_inner
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rax
+    iretq
